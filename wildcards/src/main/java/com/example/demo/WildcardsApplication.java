c158b48b01e6e4c7780abc2523be29a2c9a66692
EngineerMel <ramelhaines25@gmail.com>
2020-03-21 23:05:49 -0400

    first commit

---
+++ wildcards/src/main/java/com/example/demo/WildcardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcardsApplication.class, args);
+	}
+
+}
